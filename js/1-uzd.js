05e06d90ac43af9906ba440c3d7bf5e77ab4585e
Saulius <saulius.j@icloud.com>
2023-04-07 15:45:23 +0300

    1-uzduotis

---
+++ js/1-uzd.js
@@ -0,0 +1,16 @@
+console.clear();
+// 1. Parašyti f-ją, kuriai būtų perduodamas vienas parametras skaičius, kuris
+// padidinamas vienetu ir gražinamas rezultatas
+
+// TEST:
+// addition(0) ➞ 1
+// addition(9) ➞ 10
+// addition(-3) ➞ -2
+
+function addition(x) {
+    skaicius = x+1;
+    return skaicius;
+}
+console.log(`addition(0) -> ${addition(0)}`);
+console.log(`addition(9) -> ${addition(9)}`);
+console.log(`addition(-3) -> ${addition(-3)}`);
