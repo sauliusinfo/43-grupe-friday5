14ffef006b63a0fdef6e8349d00b5c6d6eeed80e
Saulius <saulius.j@icloud.com>
2023-04-07 18:37:50 +0300

    3-uzduotis

--- js/3-uzd.js
+++ js/3-uzd.js
@@ -2,16 +2,23 @@ console.clear();
 // 3. Parašyti f-ją, kuri skaičiuotų krepšinio taškus, pirmas perduodamas parametras baudų skaičius, 
 // antras perduodamas parametras dvitaškių skaičius, trečias perduodamas tritaškių. 
 // Apskaičiuoti ir gražinti bendrą surinkta komandos taškų skaičių. 
+// 
+// TEST:
+// basketballPoints(5, 25, 10) ➞ 85
+// basketballPoints(1, 1, 1) ➞ 6
+// basketballPoints(10, 20, 5) ➞ 65
 
 let baudos =0;
 let dvitaskiai =0;
 let tritaskiai =0;
 const bauda =1; const dvitaskis =2; const tritaskis =3;
 
-function zalgiris(baudos, dvitaskiai, tritaskiai) {
+function basketballPoints(baudos, dvitaskiai, tritaskiai) {
     komanda = baudos * bauda
             + dvitaskiai * dvitaskis
             + tritaskiai * tritaskis;
     return komanda;
 }
-console.log(`Komanda is viso surinko tasku: ${zalgiris(9, 9, 9)}`);
+console.log(`basketballPoints(5, 25, 10) ➞ ${basketballPoints(5, 25, 10)}`);
+console.log(`basketballPoints(1, 1, 1) ➞ ${basketballPoints(1, 1, 1)}`);
+console.log(`basketballPoints(10, 20, 5) ➞ ${basketballPoints(10, 20, 5)}`);
