a4694d56db075f31beed4c1dc78c96bf6752f5f7
Saulius <saulius.j@icloud.com>
2023-04-07 18:05:18 +0300

    3-uzduotis

--- js/3-uzd.js
+++ js/3-uzd.js
@@ -6,11 +6,12 @@ console.clear();
 let baudos =0;
 let dvitaskiai =0;
 let tritaskiai =0;
+const bauda =1; const dvitaskis =2; const tritaskis =3;
 
 function zalgiris(baudos, dvitaskiai, tritaskiai) {
-    komanda = baudos * 1
-            + dvitaskiai * 2
-            + tritaskiai * 3;
+    komanda = baudos * bauda
+            + dvitaskiai * dvitaskis
+            + tritaskiai * tritaskis;
     return komanda;
 }
 console.log(`Komanda is viso surinko tasku: ${zalgiris(9, 9, 9)}`);
